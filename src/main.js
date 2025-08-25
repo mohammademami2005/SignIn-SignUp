cde2534eac723db3afb9de56802b583ecbdff2c4
{MohammadEmami} <162024353+mohammademami2005@users.noreply.github.com>
2025-08-25 20:53:51 +0330

    update it

--- src/main.js
+++ src/main.js
@@ -7,12 +7,19 @@ const signUp = document.querySelector("#signUp")
 const signIn = document.querySelector("#signIn")
 
 const pass = document.querySelector("#pass")
-
+const suForm = document.querySelector("#signUp")
+const suEmail = document.getElementById("signUpEmail")
+const label = document.querySelector("#signUpEmailLabel")
+const suUsername = document.querySelector("#signUpUsername")
+const suUsernameLabel = document.querySelector("#signUpUsernameLabel")
+let submitState1 = 0
 let flag = 0
 swapBtn.addEventListener("click", (e) => {
   if (flag % 2 == 0) {
     swapBox.classList.remove("right-0")
     swapBox.classList.add("right-1/2")
+    swapBtn.style.backgroundColor = "#d3d1bc"
+    swapBtn.style.color = "black"
 
     signUp.classList.remove("opacity-0")
     signUp.classList.add("opacity-100")
@@ -24,6 +31,9 @@ swapBtn.addEventListener("click", (e) => {
   } else {
     swapBox.classList.remove("right-1/2")
     swapBox.classList.add("right-0")
+    swapBtn.style.backgroundColor = "#93b3a8"
+    swapBtn.style.color = ""
+
 
     signUp.classList.remove("opacity-100")
     signUp.classList.add("opacity-0")
@@ -36,14 +46,53 @@ swapBtn.addEventListener("click", (e) => {
   flag++
 })
 
-let x = 0;
-document.getElementById("signUpEmail").addEventListener("input", (e) => {
-  const label = document.querySelector("#signUpEmailLabel")
-  let mail = e.target.value
-  if (mail.search(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) >= 0) {
-    pass.focus()
-    
-  } else {
-    label.innerHTML = 'not found'
+suForm.addEventListener("submit", (e) => {
+  // sign up username ------------
+  let username = suUsername.value
+  if (username == '' || username.length < 5) {
+    suUsernameLabel.innerHTML = "یوزر نیم نمیتونه خالی یا کمتر از 5 کاراکتر باشد"
   }
+  // sign up username end ------------
+
+  let mail = suEmail.value.trim();
+  let mailAtLength = (mail.match(/@/g) || []).length;
+  label.innerHTML = ""; 
+
+  if (mail === "") {
+    label.append("نمیتونه خالی باشه");
+  }
+  if (mail.search(/@/) < 0) {
+    label.append("@ ندارد");
+  }
+  if (mailAtLength > 1) {
+    label.append("شما نمیتوانید از دو @ استفاده کنید");
+  }
+  if (mail.indexOf(".") < 0 || (mail.length - mail.lastIndexOf(".")) <= 2) {
+    label.append("ساختار دامنه درست نیست");
+  }
+  if (mail.length < 10) {
+    label.append("ایمیل خیلی کوتاه است");
+  }
+  if (/<script/i.test(mail) || /&lt;script/i.test(mail)) {
+    label.append("بیشرف نکن");
+  }
+   if (mail.search(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) >= 0) {
+    // submitState1++
+    label.textContent = "✅"
+  }
+
+
+  if (submitState1 < 3) {
+    e.preventDefault()
+  }
+
 })
+
+signUpEmail.addEventListener("input", (e) => {
+
+  let mail = e.target.value
+
+
+})
+
+"نمیتونه با نقطه به پایان برسه"
