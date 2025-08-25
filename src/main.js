49b9ce0222d44624d2100cea50837828c129c0da
{MohammadEmami} <162024353+mohammademami2005@users.noreply.github.com>
2025-08-25 03:40:42 +0330

    add style and features and complete a section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,37 @@
 import './style.css'
 
-document.querySelector("#app").innerHTML = `
-<h1 class="text-5xl w-screen h-screen flex justify-center items-center">hello world</h1>
-`
+
+const swapBtn = document.querySelector("#swap")
+const swapBox = document.querySelector("#swapBox")
+const signUp = document.querySelector("#signUp")
+const signIn = document.querySelector("#signIn")
+
+let flag = 0
+swapBtn.addEventListener("click", (e) => {
+
+
+  if (flag % 2 == 0) {
+    swapBox.classList.remove("right-0")
+    swapBox.classList.add("right-1/2")
+
+    signUp.classList.remove("opacity-0")
+    signUp.classList.add("opacity-100" )
+    
+    signIn.classList.remove("opacity-100"  )
+    signIn.classList.add("opacity-0")
+    swapBtn.textContent = "sign in"
+
+  } else {
+    swapBox.classList.remove("right-1/2")
+    swapBox.classList.add("right-0")
+
+    signUp.classList.remove("opacity-100")
+    signUp.classList.add("opacity-0")
+    
+    signIn.classList.remove("opacity-0")
+    signIn.classList.add("opacity-100" )
+    swapBtn.textContent = "sign up"
+
+  }
+  flag++
+})
